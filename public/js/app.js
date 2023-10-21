2a2db602ac996cf904ce646994e717a305f07ecd
porter oleson <porter.oleson@gmail.com>
2023-10-20 21:45:50 -0600

    Authentication 2

---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+let auth0Client = null;
+
+const fetchAuthConfig = () => fetch("/auth_config.json");
+
+const configureClient = async () => {
+    const response = await fetchAuthConfig();
+    const config = await response.json();
+  
+    auth0Client = await auth0.createAuth0Client({
+      domain: config.domain,
+      clientId: config.clientId
+    });
+  };
+
+  window.onload = async () => {
+    await configureClient();
+  }
