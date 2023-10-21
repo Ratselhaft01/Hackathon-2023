a3a2e587e96e517ca199c9c4377278dcc20fc439
savannahsammons <sam20003@byui.edu>
2023-10-21 04:35:11 -0600

    finish css

--- public/js/formatting.js
+++ public/js/formatting.js
@@ -3,14 +3,3 @@
 function formatText(command, value = null) {
     document.execCommand(command, false, value);
 }
-
-// function updateFormattedText() {
-//     const inputField = document.getElementById("inputField");
-//     const formattedText = document.getElementById("formattedText");
-  
-//     const inputText = inputField.value;
-//     // Apply formatting or styling to a specific part of the inputText
-//     const formattedHTML = `<span style="color: red;">${inputText.substr(0, 5)}</span>${inputText.substr(5)}`;
-//     formattedText.innerHTML = formattedHTML;
-//     formattedText.style.display = "block";
-// }
