a179aa88d655537272a12cc802c173b32e5d7317
savannahsammons <sam20003@byui.edu>
2023-10-21 02:17:15 -0600

    note styles

---
+++ public/js/formatting.js
@@ -0,0 +1,9 @@
+// Get the button and the element to be modified
+const formatButton = document.getElementById("formatButton");
+const content = document.getElementById("content");
+
+// Add a click event listener to the button
+formatButton.addEventListener("click", function() {
+  // Change the HTML content when the button is clicked
+  content.innerHTML = "Formatted Text";
+});
